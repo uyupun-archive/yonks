896acaef5ec4aa54e9662a5505105f29af32574c
takashi0602 <t.takashi0602@gmail.com>
2021-10-24 06:15:36 +0900

    feat: footer and constants file

---
+++ constants/index.tsx
@@ -0,0 +1,9 @@
+export const color = {
+  orange: '#f15a24',
+  lightOrange: '#f15b246e',
+  red: '#ff3300',
+  green: '#a1da2a',
+  blue: '#00bfff',
+  gray: '#888888',
+  white: '#ffffff',
+} as const;
