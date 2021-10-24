6f3c5cd44b9ccabee2d1b957c102fea03e5762ee
takashi0602 <t.takashi0602@gmail.com>
2021-10-24 10:00:06 +0900

    feat: register page

--- pages/register.tsx
+++ pages/register.tsx
@@ -1,38 +1,100 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
+import { useState } from 'react';
 import logo from '../assets/logo.png';
-import { StyleSheet, Text, View, Button, Image } from 'react-native';
+import {
+  StyleSheet,
+  Text,
+  SafeAreaView,
+  Button,
+  Image,
+  TouchableOpacity,
+  TextInput,
+  Dimensions,
+} from 'react-native';
 import { PageProps } from '../types';
+import { color } from '../constants';
+import { fetcher } from '../utilities/fetcher';
 
 const Register = (props: PageProps) => {
   const { navigation } = props;
 
+  const [text, setText] = useState<string>('');
+  const [password, setPassword] = useState<string>('');
+
+  const register = async () => {
+    try {
+      // TODO: APIリクエスト
+      // const res = await fetcher('', { method: 'POST' });
+      // const data = await res.json();
+      // console.log(data);
+      navigation.navigate('Profile');
+    } catch (error) {
+      console.error('エラーです');
+    }
+  };
+
   return (
-    <View style={styles.container}>
-      <Text>Register page</Text>
+    <SafeAreaView style={styles.container}>
       <Image source={logo} style={styles.logo} />
-      <Button title={'ログイン画面へ'} onPress={() => navigation.navigate('Login')} />
-      <Button title={'新規登録画面へ'} onPress={() => navigation.navigate('Register')} />
-      <Button title={'フレンド画面へ'} onPress={() => navigation.navigate('Friends')} />
-      <Button title={'プロフィール画面へ'} onPress={() => navigation.navigate('Profile')} />
-      <Button title={'通知画面へ'} onPress={() => navigation.navigate('Notice')} />
+      <Text style={styles.label}>ユーザID</Text>
+      <TextInput style={styles.input} onChangeText={setText} value={text} />
+      <Text style={styles.label}>パスワード</Text>
+      <TextInput secureTextEntry style={styles.input} onChangeText={setPassword} value={password} />
+      <TouchableOpacity
+        onPress={() => register()}
+        style={text && password ? styles.button : styles.buttonDisabled}
+        disabled={!(text && password)}
+      >
+        <Text>新規登録</Text>
+      </TouchableOpacity>
+      <Button title={'ログインする'} onPress={() => navigation.navigate('Register')} />
       <StatusBar style='auto' />
-    </View>
+    </SafeAreaView>
   );
 };
+
+const inputWidth = Dimensions.get('window').width - 40;
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    justifyContent: 'center',
+    backgroundColor: color.white,
     alignItems: 'center',
   },
-
   logo: {
     width: 250,
     height: 130,
-  }
+    marginTop: 72,
+    marginBottom: 64,
+  },
+  label: {
+    alignSelf: 'flex-start',
+    paddingHorizontal: 20,
+  },
+  input: {
+    width: inputWidth,
+    height: 40,
+    paddingHorizontal: 10,
+    borderWidth: 1,
+    borderColor: color.lightGray,
+    borderRadius: 4,
+    marginBottom: 20,
+  },
+  button: {
+    backgroundColor: color.orange,
+    paddingVertical: 16,
+    paddingHorizontal: 48,
+    borderRadius: 5,
+    marginBottom: 12,
+  },
+  buttonDisabled: {
+    backgroundColor: color.lightOrange,
+    paddingVertical: 16,
+    paddingHorizontal: 48,
+    borderRadius: 5,
+    marginBottom: 12,
+  },
 });
 
 export { Register };
