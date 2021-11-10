dab4de52a9004b8805f27d27511cd5b02dd1558f
takashi0602 <t.takashi0602@gmail.com>
2021-11-11 00:31:15 +0900

    refactor プロフィール画面をNativeBaseで実装する

--- pages/profile.tsx
+++ pages/profile.tsx
@@ -1,20 +1,21 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { useState } from 'react';
-import { StyleSheet, Text, SafeAreaView, TextInput, ScrollView, Dimensions } from 'react-native';
-import { PageProps } from '../types';
+import { ScrollView, Box, Center, Heading, Text, Select, CheckIcon } from 'native-base';
+import { Input } from '../components/input';
 import { Footer } from '../components/footer';
+import { PageProps } from '../types';
 import { useFetcher } from '../hooks/useFetcher';
-import { color } from '../constants';
-import RNPickerSelect from 'react-native-picker-select';
+import { status as statusObj } from '../constants';
+
+const data = {
+  userId: 'takashi0602',
+};
 
 const Profile = (props: PageProps) => {
   const { navigation } = props;
   // TODO: APIリクエスト
   // const { data, isLoading, isError } = useFetcher('');
-  const data = {
-    userId: 'takashi0602',
-  };
 
   const [name, setName] = useState<string>('');
   const [status, setStatus] = useState<string>('');
@@ -23,122 +24,59 @@ const Profile = (props: PageProps) => {
   const [instagram, setInstagram] = useState<string>('');
   const [tikTok, setTikTok] = useState<string>('');
 
+  const getSelectItem = () => {
+    return Object.values(statusObj).map((label, index) => {
+      return <Select.Item key={index} label={label} value={`${index + 1}`} />;
+    });
+  };
+
   return (
     <>
-      <SafeAreaView style={styles.container}>
-        <Text style={styles.header}>プロフィール</Text>
-        <ScrollView style={styles.scrollView}>
-          <Text style={styles.userId}>ユーザID: {data.userId}</Text>
-          <Text style={styles.label}>名前</Text>
-          <TextInput style={styles.input} onChangeText={setName} value={name} />
-          <Text style={styles.label}>ステータス</Text>
-          <RNPickerSelect
-            style={pickerSelectStyles}
-            onValueChange={(value) => setStatus(value)}
-            items={[
-              { label: 'いそがしい', value: '1' },
-              { label: 'どっちでも', value: '2' },
-              { label: '人肌恋しい', value: '3' },
-            ]}
-            placeholder={{ label: '選択してください', value: '' }}
-          />
-          <Text style={styles.messagingApp}>メッセージングアプリ</Text>
-          <Text style={styles.label}>LINE</Text>
-          <TextInput style={styles.input} onChangeText={setLine} value={line} />
-          <Text style={styles.label}>Twitter</Text>
-          <TextInput style={styles.input} onChangeText={setTwitter} value={twitter} />
-          <Text style={styles.label}>Instagram</Text>
-          <TextInput style={styles.input} onChangeText={setInstagram} value={instagram} />
-          <Text style={styles.label}>TikTok</Text>
-          <TextInput style={styles.lastInput} onChangeText={setTikTok} value={tikTok} />
+      <Box flex={1} bg={'white'} safeArea>
+        <Center borderColor='gray.300' borderBottomWidth={1} pt={1} pb={2}>
+          <Heading size='sm'>プロフィール</Heading>
+        </Center>
+        <ScrollView _contentContainerStyle={{ pt: 6, px: 8 }} mb={12}>
+          <Text fontSize={'md'} mb={6}>
+            ユーザID: {data.userId}
+          </Text>
+          <Input text={'名前'} value={name} onChangeText={setName} mb={4} />
+          <Box mb={8}>
+            <Text fontSize={'md'} mb={1}>
+              ステータス
+            </Text>
+            <Select
+              h={'40px'}
+              selectedValue={status}
+              accessibilityLabel={'選択してください'}
+              placeholder={'選択してください'}
+              _item={{
+                _pressed: {
+                  backgroundColor: 'orange.300',
+                },
+              }}
+              _selectedItem={{
+                bg: 'orange.100',
+                // endIcon: <CheckIcon size='5' />,
+              }}
+              onValueChange={(value) => setStatus(value)}
+            >
+              {getSelectItem()}
+            </Select>
+          </Box>
+          <Heading size='sm' mb={4}>
+            メッセージングアプリ
+          </Heading>
+          <Input text={'LINE'} value={line} onChangeText={setLine} mb={4} />
+          <Input text={'Twitter'} value={twitter} onChangeText={setTwitter} mb={4} />
+          <Input text={'Instagram'} value={instagram} onChangeText={setInstagram} mb={4} />
+          <Input text={'TikTok'} value={tikTok} onChangeText={setTikTok} mb={8} />
           <StatusBar style='auto' />
         </ScrollView>
-      </SafeAreaView>
-      <Footer active={'profile'} navigation={navigation} />
+        <Footer active={'profile'} navigation={navigation} />
+      </Box>
     </>
   );
 };
-
-const inputWidth = Dimensions.get('window').width - 40;
-const scrollViewHeight = Dimensions.get('window').height - 100;
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: color.white,
-    alignItems: 'center',
-  },
-  scrollView: {
-    width: '100%',
-    height: scrollViewHeight,
-  },
-  header: {
-    width: '100%',
-    textAlign: 'center',
-    fontSize: 16,
-    paddingVertical: 12,
-    borderBottomColor: color.gray,
-    borderBottomWidth: 1,
-    marginBottom: 20,
-  },
-  userId: {
-    alignSelf: 'flex-start',
-    paddingHorizontal: 20,
-    marginBottom: 20,
-  },
-  label: {
-    alignSelf: 'flex-start',
-    paddingHorizontal: 20,
-    marginBottom: 4,
-  },
-  messagingApp: {
-    alignSelf: 'flex-start',
-    paddingHorizontal: 20,
-    marginBottom: 20,
-  },
-  input: {
-    width: inputWidth,
-    height: 40,
-    paddingHorizontal: 10,
-    borderWidth: 1,
-    borderColor: color.lightGray,
-    borderRadius: 4,
-    marginBottom: 20,
-    alignSelf: 'center',
-  },
-  lastInput: {
-    width: inputWidth,
-    height: 40,
-    paddingHorizontal: 10,
-    borderWidth: 1,
-    borderColor: color.lightGray,
-    borderRadius: 4,
-    marginBottom: 74,
-    alignSelf: 'center',
-  },
-});
-
-const pickerSelectStyles = StyleSheet.create({
-  inputIOS: {
-    width: inputWidth,
-    height: 40,
-    paddingHorizontal: 10,
-    borderWidth: 1,
-    borderColor: color.lightGray,
-    borderRadius: 4,
-    marginBottom: 20,
-    alignSelf: 'center',
-  },
-  inputAndroid: {
-    width: inputWidth,
-    height: 40,
-    paddingHorizontal: 10,
-    borderWidth: 1,
-    borderColor: color.lightGray,
-    borderRadius: 4,
-    marginBottom: 20,
-    alignSelf: 'center',
-  },
-});
 
 export { Profile };
